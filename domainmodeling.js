8fe293049c292ed59bdd3fb172446e49c584969e
chris held <chris.held@zyquest.com>
2018-03-04 07:42:28 -0600

    initial

---
+++ domainmodeling.js
@@ -0,0 +1,10 @@
+//model out the following domain (JSON would probably be easiest, but 
+//do whatever you are comfortable with)
+
+//We want to create a platform that organizations can use to sell
+//their products online. Organizations would log into our platform and set up
+//stores under their organization. Each store would have it's own products,
+//but we would like the ability to share products across stores within
+//an organization. We would like our customers to be able to save the status
+//of their cart on individual stores, and allow them to create a wishlist that can
+//hold items from multiple stores across an organization.
---
+++ fizzbuzz.js
@@ -0,0 +1,5 @@
+//write a program that prints the numbers
+//from 1 to 100. If the number is divisible by 3,
+//also print "Fizz". If the number is divisible by 5,
+//also print "Buzz". If the number is divisible by both
+//3 and 5, also print "FizzBuzz"
